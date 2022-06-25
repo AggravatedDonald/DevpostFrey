7ead44c548c19215cc61c396bee2be4f27585ab5
Manish Kandepalli <79335262+manish295@users.noreply.github.com>
2022-06-25 14:02:49 -0400

    scrape another website

--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-import json
+import re
 
 
 def scrape(id, location="", age="All", purpose="All"):
@@ -10,51 +10,75 @@ def scrape(id, location="", age="All", purpose="All"):
     soup = BeautifulSoup(doc, "html.parser")
     events_div = soup.find("div", class_="view view-event-locator-npld view-id-event_locator_npld view-display-id-attachment_2 event-details")
     events_div = events_div.findChild('div', "view-content")
-    print(events_div.prettify())
-    if events_div == None:
-        return False
-    events = events_div.findChildren('div', recursive=False)
-    for event in events:
-        event_details = event.find("div", class_="event-description")
-        name = event_details.find("h4").text
-        event_link = event_details.find("div", class_="field-link").find('a')['href']
-        new_doc = requests.get("https://www.neefusa.org"+ event_link).text
-        new_soup = BeautifulSoup(new_doc, "html.parser")
-        event_details = new_soup.find("article").findChild("div", "content clearfix")
-        event_paragraph = event_details.findChild("div", "field field-name-field-event-description field-type-text-long field-label-hidden").findChild("div").findChild("div").text #NEED
-        event_date_div = event_details.findChild("div", "field field-name-field-date-and-time field-type-datetime field-label-above")
-        date = event_date_div.find("span").text #NEED
-        time_start = event_date_div.find("span", class_="date-display-start").text #NEED
-        time_end = event_date_div.find("span", class_="date-display-end").text #NEED
-        location_div = event_details.findChild("div", "field field-name-field-address field-type-addressfield field-label-above")
-        if location_div != None:
-            location = location_div.findChild("div", "field-item even").text #NEED
-        else:
-            location = None
-        host_links = event_details.findChild("div", "event-social-links").findChildren("div", recursive=False)
-        links = []
-        for link in host_links:
-            link = link.find("a")["href"]
-            links.append(link)
+    if events_div != None:
+        events = events_div.findChildren('div', recursive=False)
+        for event in events:
+            event_details = event.find("div", class_="event-description")
+            name = event_details.find("h4").text
+            event_link = event_details.find("div", class_="field-link").find('a')['href']
+            new_doc = requests.get("https://www.neefusa.org"+ event_link).text
+            new_soup = BeautifulSoup(new_doc, "html.parser")
+            event_details = new_soup.find("article").findChild("div", "content clearfix")
+            event_paragraph = event_details.findChild("div", "field field-name-field-event-description field-type-text-long field-label-hidden").findChild("div").findChild("div").text #NEED
+            event_date_div = event_details.findChild("div", "field field-name-field-date-and-time field-type-datetime field-label-above")
+            date = event_date_div.find("span").text #NEED
+            time_start = event_date_div.find("span", class_="date-display-start").text #NEED
+            time_end = event_date_div.find("span", class_="date-display-end").text #NEED
+            location_div = event_details.findChild("div", "field field-name-field-address field-type-addressfield field-label-above")
+            if location_div != None:
+                location = location_div.findChild("div", "field-item even").text #NEED
+            else:
+                location = None
+            host_links = event_details.findChild("div", "event-social-links").findChildren("div", recursive=False)
+            links = []
+            for link in host_links:
+                link = link.find("a")["href"]
+                links.append(link)
 
+            
+            org_name = event_details.findChild("div", "field field-name-field-organization field-type-text field-label-hidden")
+            if org_name != None:
+                org_name = org_name.text
+            contact_title = event_details.findChild("div", "field field-name-field-job-title field-type-text field-label-hidden")
+            if contact_title != None:
+                contact_title = contact_title.text
+            contact_email = event_details.findChild("div", "field field-name-field-email-address field-type-email field-label-hidden")
+            if contact_email != None:
+                contact_email = contact_email.text
+            contact_phone = event_details.findChild("div", "field field-name-field-phone-number-text field-type-text field-label-hidden")
+            if contact_phone != None:
+                contact_phone = contact_phone.text
+            contact_name = event_details.findChild("div", "strong")
+            if contact_name != None:
+                contact_name = contact_name.text
+            info.append({"_id": id, "event_name": name, "event_desc": event_paragraph, "location": location, "date": date, "host_links": links, "org_name": org_name, "contact_title": contact_title, "contact_email": contact_email, "contact_phone": contact_phone, "contact_name": contact_name})
         
-        org_name = event_details.findChild("div", "field field-name-field-organization field-type-text field-label-hidden")
-        if org_name != None:
-            org_name = org_name.text
-        contact_title = event_details.findChild("div", "field field-name-field-job-title field-type-text field-label-hidden")
-        if contact_title != None:
-            contact_title = contact_title.text
-        contact_email = event_details.findChild("div", "field field-name-field-email-address field-type-email field-label-hidden")
-        if contact_email != None:
-            contact_email = contact_email.text
-        contact_phone = event_details.findChild("div", "field field-name-field-phone-number-text field-type-text field-label-hidden")
-        if contact_phone != None:
-            contact_phone = contact_phone.text
-        contact_name = event_details.findChild("div", "strong")
-        if contact_name != None:
-            contact_name = contact_name.text
-        info.append({"_id": id,  "event_name": name, "event_desc": event_paragraph, "date": date, "host_links": links, "org_name": org_name, "contact_title": contact_title, "contact_email": contact_email, "contact_phone": contact_phone, "contact_name": contact_name})
     
-    data = json.dumps(info)
-    requests.post("http://localhost:8000/output", json=data)
-
+    url = f"https://www.eventbrite.com/d/{location}/free--charity-and-causes--events/environment/?end_date=2022-09-22&page=1&start_date=2022-07-01"
+    doc2 = requests.get(url).text
+    soup2 = BeautifulSoup(doc2, "html.parser")
+    main_content_div = soup2.find("div", class_="search-main-content")
+    events_li = main_content_div.findChild("ul", recursive=False).findChildren("li", recursive=False)
+    for event in events_li:
+        event_page = event.findChild("a")["href"]
+        event_page_doc = requests.get(event_page).text
+        soup3 = BeautifulSoup(event_page_doc, "html.parser")
+        title = soup3.find("h1", class_="listing-hero-title").text #NEED
+        event_info_div = soup3.find("div", class_="event-info")
+        event_paragraph = event_info_div.findChild("div", "has-user-generated-content").text #NEED
+        location_div = event_info_div.findChild("section", attrs={"aria-labelledby": "location-heading"})
+        location = location_div.text
+        location = location.replace("Location", "")
+        location = location.replace("View map", "") 
+        location =  re.sub('([a-z])([A-Z])', r'\1 \2', location) #NEED
+        start_date = soup3.find("div", class_="g-group").findChild("time").text #NEED
+        org_profile_div = soup3.find("a", attrs={"id": "organizer-link-org-panel"})
+        if org_profile_div == None:
+            org_name = None #NEED
+        else:
+            org_name = org_profile_div.text.lstrip().rstrip() #NEED
+        
+        link = event_page
+        info.append({"_id": id, "event_name": title, "event_desc": event_paragraph, "location": location, "date": start_date, "host_links": [link], "org_name": org_name, "contact_title": None, "contact_email": None, "contact_phone": None, "contact_name": None})
+        
+    requests.post("http://localhost:8000/output", json=info)
